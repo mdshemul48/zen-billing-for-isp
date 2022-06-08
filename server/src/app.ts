ee8cd52cff8db388929cb03b8eeb96b5014face8
MD. Shimul <mdshemul480@gmail.com>
2022-06-09 00:39:34 +0600

    adding force false

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-sequelize.sync().then(async () => {
+sequelize.sync({ force: false }).then(async () => {
   await mainSeederFunc();
   app.listen(port, () => {
     console.log(`api working on http://localhost:${port}`);
