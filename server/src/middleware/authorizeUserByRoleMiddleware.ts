4db69d96c03a7f9599e95cac5c525cd180b1bc4d
MD. Shimul <mdshemul480@gmail.com>
2022-06-10 00:47:21 +0600

    removing just log

--- server/src/middleware/authorizeUserByRoleMiddleware.ts
+++ server/src/middleware/authorizeUserByRoleMiddleware.ts
@@ -4,7 +4,6 @@ import modifiedRequest from "../types/modifiedRequest";
 
 const authorizeUserByRole = (role: userInterface["role"][]) => {
   return (req: modifiedRequest, res: Response, next: NextFunction) => {
-    console.log(req.user);
     if (!role.includes(req.user!.role)) {
       return res.status(401).json({
         msg: "You are not authorized to perform this action",
