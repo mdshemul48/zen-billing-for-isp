0f50f4bd7a384fdc838cdafdbb1d4c3794be9599
MD. Shimul <mdshemul480@gmail.com>
2022-06-09 22:22:43 +0600

    adding new middleware

---
+++ server/src/middleware/authentication.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../model/User";
+import userInterface from "../types/UserType";
+
+interface modifiedRequest extends Request {
+  user: userInterface;
+}
+
+const authentication = async (req: modifiedRequest, res: Response, next: NextFunction) => {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+  try {
+    const decoded = <userInterface>jwt.verify(token, process.env.JWT_SECRET!);
+    console.log(decoded);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
+
+export default authentication;
