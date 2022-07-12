91823f078378732ef41c5b0b79d638bd209550c1
MD. Shimul <mdshemul480@gmail.com>
2022-07-12 23:36:28 +0600

    adding mikrotik model

---
+++ server/src/model/MikroTik.ts
@@ -0,0 +1,59 @@
+import sequelize from "../config/DB";
+import Sequelize from "sequelize";
+
+class MikroTik extends Sequelize.Model {
+  readonly id!: number;
+  name!: string;
+  ip!: string;
+  username!: string;
+  password!: string;
+  port!: number;
+  remarks?: string;
+  readonly createdAt!: Date;
+  readonly updatedAt!: Date;
+}
+
+MikroTik.init(
+  {
+    id: {
+      allowNull: false,
+
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    ip: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    port: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    remarks: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+  },
+
+  {
+    sequelize,
+    modelName: "MikroTik",
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
+);
+
+export default MikroTik;
