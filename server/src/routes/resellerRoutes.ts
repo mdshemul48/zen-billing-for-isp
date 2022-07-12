35d6a031615516579471a049a96cd84287e89b53
MD. Shimul <mdshemul480@gmail.com>
2022-07-12 22:36:53 +0600

    adding set reseller User func

--- server/src/routes/resellerRoutes.ts
+++ server/src/routes/resellerRoutes.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
-import { createReseller, resellerValidation } from "../controllers/reseller/reseller";
 import authentication from "../middleware/authenticationMiddleware";
 import authorizeUserByRole from "../middleware/authorizeUserByRoleMiddleware";
+
+import {
+  createReseller,
+  resellerValidation,
+  setResellerUser,
+  setResellerUserValidation,
+} from "../controllers/reseller/reseller";
 
 const router = Router();
 
 router.route("/").post(authentication, authorizeUserByRole(["admin"]), resellerValidation, createReseller);
+router
+  .route("/setUserToReseller")
+  .post(authentication, authorizeUserByRole(["admin"]), setResellerUserValidation, setResellerUser);
 
 export default router;
