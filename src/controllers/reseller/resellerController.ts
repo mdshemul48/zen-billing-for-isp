8f9bc64acc84f677af4bb9c1d7d1f20b2238e4a9
MD. Shimul <mdshemul480@gmail.com>
2022-08-12 13:38:50 +0600

    adding set reseller user to another controller

--- src/controllers/reseller/resellerController.ts
+++ src/controllers/reseller/resellerController.ts
@@ -3,9 +3,6 @@ import { body, validationResult } from "express-validator";
 import ResellerType from "../../types/ResellerType";
 
 import Reseller from "../../model/Reseller";
-import User from "../../model/User";
-import ResellerUser from "../../model/ResellerUser";
-import modifiedRequest from "../../types/modifiedRequest";
 
 // @route   POST api/reseller
 // @desc    creating Reseller
@@ -31,101 +28,6 @@ export const createReseller = async (req: Request, res: Response) => {
       errors: [
         {
           msg: "something went wrong while creating Reseller.",
-        },
-      ],
-    });
-  }
-};
-
-// @route   POST api/reseller/setUserToReseller
-// @desc    setting user with reseller
-// @access  Private (only for admin)
-export const setResellerUserValidation = [
-  body("reseller")
-    .isInt()
-    .withMessage("Reseller id Not Passed")
-    .customSanitizer(async (ResellerId) => {
-      const reseller = await Reseller.findOne({
-        where: {
-          id: ResellerId,
-        },
-      });
-      if (reseller) {
-        return reseller;
-      }
-      return null;
-    })
-    .custom((value) => {
-      if (value) {
-        return true;
-      }
-      throw new Error("Reseller doesn't exist with provided reseller id.");
-    }),
-
-  body("user")
-    .isInt()
-    .withMessage("User Id not provided")
-    .customSanitizer(async (userId) => {
-      const user = await User.findOne({
-        where: {
-          id: userId,
-        },
-      });
-      if (user) {
-        return user;
-      }
-      return null;
-    })
-    .custom((value) => {
-      if (value) {
-        return true;
-      }
-      throw new Error("User doesn't exist with provided user id.");
-    }),
-];
-
-export const setResellerUser = async (req: modifiedRequest, res: Response) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
-
-  const { user, reseller } = <{ user: User; reseller: Reseller }>req.body;
-
-  if (user.role == "admin") {
-    return res.status(406).json({
-      errors: [
-        {
-          msg: "Admin don't need to assign any reseller.",
-        },
-      ],
-    });
-  }
-
-  try {
-    const alreadyCreated = await ResellerUser.findOne({
-      where: {
-        resellerId: reseller.id,
-        userId: user.id,
-      },
-    });
-    if (alreadyCreated) {
-      return res.status(409).json({
-        errors: [
-          {
-            msg: "User already connect to the reseller.",
-          },
-        ],
-      });
-    }
-    await ResellerUser.create({
-      userId: user.id,
-      resellerId: reseller.id,
-    });
-    return res.status(201).json({ msg: "User and Reseller connection successfully created!" });
-  } catch (error) {
-    return res.status(500).json({
-      errors: [
-        {
-          msg: "Something went wrong while create user and reseller connection.",
         },
       ],
     });
--- src/routes/resellerRoutes.ts
+++ src/routes/resellerRoutes.ts
@@ -2,12 +2,9 @@ import { Router } from "express";
 import authentication from "../middleware/authenticationMiddleware";
 import authorizeUserByRole from "../middleware/authorizeUserByRoleMiddleware";
 
-import {
-  createReseller,
-  resellerValidation,
-  setResellerUser,
-  setResellerUserValidation,
-} from "../controllers/reseller/resellerController";
+import { createReseller, resellerValidation } from "../controllers/reseller/resellerController";
+import { setResellerUser, setResellerUserValidation } from "../controllers/reseller/resellerUserController";
+import { resellerBalanceRecharge } from "../controllers/reseller/resellerBalanceController";
 
 const router = Router();
 
@@ -16,4 +13,5 @@ router
   .route("/setUserToReseller")
   .post(authentication, authorizeUserByRole(["admin"]), setResellerUserValidation, setResellerUser);
 
+router.route("/resellerBalance").post(authentication, authorizeUserByRole(["admin"]), resellerBalanceRecharge);
 export default router;
